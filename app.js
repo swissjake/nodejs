c5f03c2276891204de4a871c0d640c960dda4d3f
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2022-10-01 17:30:28 +0100

    ejs

--- app.js
+++ app.js
@@ -3,27 +3,41 @@ const express = require("express");
 //express app
 
 const app = express();
+app.set("view engine", "ejs");
 
 //listen for request
 app.listen(3001);
+
 app.get("/", (req, res) => {
   //   res.send("<p> home page</p>");
-  res.sendFile("/views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   //   res.send("<p> home page</p>");
-  res.sendFile("/views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-//redirects
-
-app.get("/about-me", (req, res) => {
-  res.redirect("./views/about.html");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "create a new blog" });
 });
 
 //404
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
--- test.js
+++
@@ -1,5 +0,0 @@
-const name = "SOFIRI";
-console.log(name);
-
-const names = ["s", "a", "a"];
-console.log(names[1]);
